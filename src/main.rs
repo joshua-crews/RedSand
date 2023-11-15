87086d7d50b0ebc52b60960ca196a3bc2cd5abc9
Joshua Crews <86257459+joshua-crews@users.noreply.github.com>
2023-11-15 14:34:02 -0800

    Added scene with lighting and setup split to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+mod setup;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup::setup)
+        .add_systems(Update, draw_cursor)
+        .run();
+}
+
+fn draw_cursor(
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    ground_query: Query<&GlobalTransform, With<setup::Ground>>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let ground = ground_query.single();
+
+    let Some(cursor_position) = windows.single().cursor_position() else {
+        return;
+    };
+
+    let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+        return;
+    };
+
+    let Some(distance) = ray.intersect_plane(ground.translation(), ground.up()) else {
+        return;
+    };
+    let point = ray.get_point(distance);
+
+    gizmos.circle(point + ground.up() * 0.01, ground.up(), 0.2, Color::WHITE);
 }
